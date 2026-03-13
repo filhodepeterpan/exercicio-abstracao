cd949c56f23729893a9d11fb0749f73b36e8f32d
Douglas Soares <douglassoares.cinema@gmail.com>
2026-03-13 18:41:58 -0300

    add_class_PRAIA

--- src/br/edu/fatecpg/tecprog/abstracao/view/Main.java
+++ src/br/edu/fatecpg/tecprog/abstracao/view/Main.java
@@ -1,7 +1,32 @@
 package br.edu.fatecpg.tecprog.abstracao.view;
+import br.edu.fatecpg.tecprog.abstracao.model.Praia;
 
 public class Main {
     public static void main(String [] args){
+        // PRAIA
+        Praia praiaOcian = new Praia();
+        Praia praiaGuilhermina = new Praia();
+
+        praiaOcian.nome = "Praia Ocian";
+        praiaOcian.bairro = "Ocian";
+        praiaOcian.nivelDoMar = 0.75;
+        praiaOcian.extensao = 1.7;
+        praiaOcian.forcaDasOndas = "Média";
+
+        praiaGuilhermina.nome = "Praia Guilhermina";
+        praiaGuilhermina.bairro = "Guilhermina";
+        praiaGuilhermina.nivelDoMar = 0.80;
+        praiaGuilhermina.extensao = 1.5;
+        praiaGuilhermina.forcaDasOndas = "Forte";
+
+        praiaOcian.apresentar();
+        praiaGuilhermina.apresentar();
+
+        praiaOcian.indicarPerigo();
+        praiaGuilhermina.monitorarBanhistas();
+
+       // PRAÇA
+
 
     }
 }
